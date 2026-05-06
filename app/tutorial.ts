73f8aee0e46191727def4646041c0675c19e1cf0
jaydeb-ds <das294761@gmail.com>
2026-05-06 20:19:19 +0530

    typeScript 1 (intro)

---
+++ app/tutorial.ts
@@ -0,0 +1,36 @@
+// String 
+let std_name : string;
+std_name = "jaydeb";
+
+// integer
+let age:number;
+age = 31;
+
+// float 
+let cgpa : number;
+cgpa = 8.2;
+
+// Boolean 
+let pass : boolean
+pass = true
+
+// array (number)
+let marks : number[];
+marks = [96,86,68]
+
+// array (string)
+let subject : string[];
+subject = ["dsa","oops","dbms"];
+
+// tuple 
+let job : [string,number,boolean]
+job = ["web developer",12,true] 
+//if any of the field is empty then it will show error, or if the type of value and the type of variable is not match then it will also show error
+
+// any
+let des : any
+des = true
+des = 33
+des = "good"
+// any type is default type of variable, user can write any thing in this (number, string, boolean etc) any thing it will not show any error
+
