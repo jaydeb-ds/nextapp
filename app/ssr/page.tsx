d7599d17a66899b9933e61cdd3a66d1fb1f92d8d
jaydeb-ds <das294761@gmail.com>
2026-05-09 23:27:43 +0530

    final

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -1,27 +1,21 @@
 // SSR : Server Side Rendering
-
-import React from 'react'
+import React from "react";
 
 async function page() {
+  let responce = await fetch("http://localhost:3000/api/user", {
+    cache: "no-store", // no data will be stored in cache
+  });
 
-    let responce = await fetch('http://localhost:3000/api/user',{
-        cache :"no-store" // no data will be stored in cache
-    })
+  let data = await responce.json();
+  console.log("data : ", data);
 
-    console.log(responce);
-    
-    let data = await responce.json()
-
-    console.log("data : ",data);
-    
-    return (
+  return (
     <div>
       <h1>SSR : Server Side Rendering Page </h1>
       <h1>message : {data.message}</h1>
       <h1>name : {data.name}</h1>
       <h1>age : {data.age}</h1>
     </div>
-)
+  );
 }
-
-export default page
+export default page;
